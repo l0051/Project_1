6f780fbf0681591553dd985ee709940818d339a6
lusine <luskivi218@gmail.com>
2022-10-14 16:53:26 +0400

    overflow fixed x2

--- scripts/reversenumber.sh
+++ scripts/reversenumber.sh
@@ -6,10 +6,9 @@ error_m_one_arg='Must be one argument'
 error_m_number='An argument should be an integer'
 
 maxI=$( echo "$(printf "%u" -1)" / 2  | bc ) 
-minI=$((maxI*-1))
 
 error_m_too_big="An argument and it's reverse should be\
-less or equal than $maxI and more or equal than $minI"
+less or equal than $maxI and more or equal than -$maxI"
 
 
 if [ $# != 1 ]
@@ -45,15 +44,15 @@ answer=0
 
 while ! [ "$number" -eq 0 ]
 do
+	if [ $(((${answer#-} * 10) - maxI)) -gt 0 ]
+	then
+        	echo "$error_m_too_big"
+        	exit
+	fi
+
 	((answer*=10))
 	((answer+=$((number%10))))
 	((number/=10))
 done
-
-if [ $((${answer#-} - maxI)) -gt 0 ]
-then
-        echo "$error_m_too_big"
-        exit
-fi
 
 echo "$answer"
